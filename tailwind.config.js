97f4e433cd16dddaaeade40bbce8b6c3254ef114
CrisDeLynch <cpolos02@iesalbarregas.es>
2025-05-19 13:24:45 +0200

    Inicialización del proyecto
    Configuración del index.html, añadido el gitignore, también se añadió  la configuración de Tailwind al proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}"
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      screens: {
+        custom: "940px", 
+      }
+    },
+  },
+  darkMode: "class",
+  plugins: [],
+}
